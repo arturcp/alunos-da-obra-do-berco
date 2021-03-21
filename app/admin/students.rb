cdb7d94ae60758251448fe3d4451d88fd952a7b0
gabriel-murakami <56936297+gabriel-murakami@users.noreply.github.com>
2021-03-21 14:55:20 -0300

    Update app/admin/students.rb
    Co-authored-by: Artur Caliendo Prado <artur.prado@gmail.com>

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -3,7 +3,8 @@ ActiveAdmin.register Student do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 
-  permit_params :name, :description, :email, :phone, :mobile_phone, :address, :gender, :birth_date, :website, :twitter, :instagram, :facebook, :observation, :youtube_video, :status, :avatar
+  permit_params :name, :description, :email, :phone, :mobile_phone, :address, 
+    :gender, :birth_date, :website, :twitter, :instagram, :facebook, :observation, :youtube_video, :status, :avatar
 
   index do
     id_column
